61a37e06ac0ad3b34dce1fa46db192e85c49a933
ArtyomKorn1lov <85835332+ArtyomKorn1lov@users.noreply.github.com>
2023-01-14 16:07:29 +0300

    Добавил по заданию пункты меню

---
+++ partnyeram/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"/partnyeram/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнёром", 
+		"/partnyeram/become_partner.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partnyeram/raspisanie-meropriyatiy/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ partnyeram/raspisanie-meropriyatiy/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Анонсы", 
+		"/partnyeram/raspisanie-meropriyatiy/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Архив", 
+		"/partnyeram/raspisanie-meropriyatiy/archive.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
